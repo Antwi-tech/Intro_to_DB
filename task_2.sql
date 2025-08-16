98db185a5338e72aa71547a1203336062857b502
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-08-16 17:18:30 +0000

    creating tables

--- task_2.sql
+++ task_2.sql
@@ -1,38 +1,44 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT NOT NULL AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL,
+    PRIMARY KEY (author_id)
+) ENGINE=INNODB;
 
-CREATE TABLE books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT NOT NULL AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT,
+    PRIMARY KEY (customer_id)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
+    PRIMARY KEY (book_id),
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
-);
+) ENGINE=INNODB;
 
-CREATE TABLE customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL UNIQUE,
-    address TEXT
-);
-
-CREATE TABLE orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT NOT NULL AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
+    PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
+) ENGINE=INNODB;
 
-CREATE TABLE order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS order_details (
+    orderdetailid INT NOT NULL AUTO_INCREMENT,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
+    PRIMARY KEY (orderdetailid),
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+) ENGINE=INNODB;
