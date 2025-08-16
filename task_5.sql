d65d1734065ef5bdd5403672e9e0c4a7022e9013
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-08-16 17:33:33 +0000

    insertions

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES
+    (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'),
+    (2, 'Ama Serwaa', 'aserwaa@mail.com', '45 Palm Street'),
+    (3, 'Kwame Mensah', 'kmensah@gmail.com', '78 Cocoa Road'),
+    (4, 'Akua Addo', 'aaddo@yahoo.com', '12 River Lane'),
+    (5, 'Yaw Boateng', 'yboateng@hotmail.com', '90 Market Circle');
